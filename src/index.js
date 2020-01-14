4ecc05d8c9597ee0c7a1bc06fbe5cc9f44bc60de
khomych <tsirkuny@gmail.com>
2020-01-14 17:21:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const checkbox = document.querySelectorAll('.filter-check_checkbox');
 const btnCart = document.getElementById('cart');
 const cartModal = document.querySelector('.cart');
 
+const cards = document.querySelectorAll('.goods .card');
+
 checkbox.forEach((item) => {
     item.addEventListener(('change'), function() {
         if (item.checked) {
@@ -21,6 +23,7 @@ checkbox.forEach((item) => {
 
 btnCart.addEventListener('click', () => {
     cartModal.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
 });
 
 cartModal.addEventListener(('click'), (event) => {
@@ -28,6 +31,17 @@ cartModal.addEventListener(('click'), (event) => {
     const target = event.target;
     if (target.classList.contains('cart') || target.classList.contains('cart-close')) {
         cartModal.style.display = 'none';
+        document.body.style.overflow = '';
     }
 });
 //end корзина
+
+//работа с товаром
+cards.forEach(card => {
+    const btn = card.querySelector('button');
+    btn.addEventListener('click', () => {
+
+
+    });
+});
+//end работа с товаром
