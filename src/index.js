30bb2afd07a9ce96865a617855e21a719d2b8163
khomych <tsirkuny@gmail.com>
2020-01-14 15:10:28 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 'use sctrict';
 
 //checkbox
-const checkbox = document.getElementById('discount-checkbox');
+const checkbox = document.querySelectorAll('.filter-check_checkbox');
 const cart = document.getElementById('cart');
 const cartModal = document.querySelector('.cart');
 
-
-checkbox.addEventListener(('change'), function() {
-    if (this.checked) {
-        this.nextElementSibling.classList.add('checked');
-    } else {
-        this.nextElementSibling.classList.remove('checked');
-    }
-
+checkbox.forEach((item) => {
+    item.addEventListener(('change'), function() {
+        if (item.checked) {
+            item.nextElementSibling.classList.add('checked');
+        } else {
+            item.nextElementSibling.classList.remove('checked');
+        }
+    });
 });
+
 
 cart.addEventListener('click', () => {
     cartModal.style.display = 'flex';
+});
+
+cartModal.addEventListener(('click'), (event) => {
+
+    const target = event.target;
+    if (target.classList.contains('cart') || target.classList.contains('cart-close')) {
+        cartModal.style.display = 'none';
+    }
 });
 //end checkbox
